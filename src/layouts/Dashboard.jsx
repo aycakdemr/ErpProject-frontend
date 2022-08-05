0da88d55ee00c640892ddafa4b61f891e53c9913
Ayça Akdemir <mabeaaa@gmail.com>
2022-08-05 17:12:37 +0300

    navbar oluşturuldu

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Branch from './Branch'
+import Navi from './Navi'
+
+export default function Dashboard() {
+  return (
+    <div>
+      anasayfa
+      <Navi></Navi>
+      <Branch></Branch>
+    </div>
+  )
+}
